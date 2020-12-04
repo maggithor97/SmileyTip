3d72b1d52a873e75327ad22d68ca9848095b159a
maggithor97 <maggithor1@gmail.com>
2020-12-04 16:25:03 +0000

    Balance complete

--- src/bot/balance.js
+++ src/bot/balance.js
@@ -1,18 +1,17 @@
-
-re('./helpFunctions');
+var help = require('./helpFunctions');
 var smileyCoin = require('../smiley/smilecoin-cli');
 
 module.exports = {
-	   Main function
-	   balance: function (userId) {
-	       var payerAddr = help.idToAddress(userId);
-	           var unspentList = smileyCoin.getListUnspent(payerAddr);
-	
-	               unspentList.then(function (result) {
-	                     var payersBalance = help.getBalance(JSON.parse(result));
-	                           var bot = require('./bot.js');
-	                                 bot.sendBalance(payersBalance);
-	                                       return payersBalance;
-	                                           });
-	                                             }
-	                                             };
+  // Main function
+  balance: function (userId) {
+    var payerAddr = help.idToAddress(userId);
+    var unspentList = smileyCoin.getListUnspent(payerAddr);
+
+    unspentList.then(function (result) {
+      var payersBalance = help.getBalance(JSON.parse(result));
+      var bot = require('./bot.js');
+      bot.sendBalance(payersBalance);
+      return payersBalance;
+    });
+  }
+};
